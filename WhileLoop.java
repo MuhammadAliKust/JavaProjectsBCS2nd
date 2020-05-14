f047f5ad11c2a3d8f27d1cef5b445d307ed85cd2
MuhammadAliKust <55309828+MuhammadAliKust@users.noreply.github.com>
2020-05-14 11:15:34 +0500

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whileloop;
+
+/**
+ *
+ * @author NC
+ */
+public class WhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 1;
+        while(a<=10){
+            //2 * 1 =
+            System.out.println("2 " + " * "+ a + " = " + 2*a);
+            a++;
+        }
+    }
+    
+}
