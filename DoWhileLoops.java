589a51eb7f2f39e258cb1a3be24326ee5b7eafd3
MuhammadAliKust <55309828+MuhammadAliKust@users.noreply.github.com>
2020-05-15 11:17:39 +0500

    Add files via upload

---
+++ DoWhileLoops.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dowhileloops;
+
+/**
+ *
+ * @author NC
+ */
+public class DoWhileLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 1;
+        do{
+            System.out.println("This is a simple statement.");
+            a++;
+        }while(a<10);
+    }
+    
+}
